e8a3fd79b27358116affd54a041a649a9ead1be0
Nosa Cikal Daputra <nosacikal@gmail.com>
2019-04-04 08:45:10 +0700

    Menambahkan asset dan identitas di class utama

--- app/src/main/java/com/nosacikal/pretest_akb2_10116062/MainActivity.java
+++ app/src/main/java/com/nosacikal/pretest_akb2_10116062/MainActivity.java
@@ -3,6 +3,13 @@ package com.nosacikal.pretest_akb2_10116062;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+* Nosa Cikal Daputra
+* 10116062
+* AKB-2 (IF-2)
+*
+* */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
