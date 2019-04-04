d751526d7592e93b5ecff0734fb48cd10a2f43ad
Nosa Cikal Daputra <nosacikal@gmail.com>
2019-04-04 08:34:31 +0700

    Membuat project baru

---
+++ app/src/main/java/com/nosacikal/pretest_akb2_10116062/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nosacikal.pretest_akb2_10116062;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
